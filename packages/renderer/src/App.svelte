b54fe47f4aeb2656203a4893cca31dc28e9374c3
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-08-06 14:09:17 +0300

    Init the renderer package (the UI) with Svelte + shadcn/ui

---
+++ packages/renderer/src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { ModeWatcher } from "mode-watcher";
+  import { Toaster } from "$lib/components/shadcn/sonner";
+</script>
+
+<main
+  class="max-w-7xl w-full mx-auto px-3 flex flex-col h-screen justify-center"
+>
+  <Toaster richColors />
+  <ModeWatcher />
+</main>
