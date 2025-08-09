42aa8a093a110f1b82b850d7f7a4d814a00fc7df
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-08-09 18:32:24 +0300

    Expose `kvStoresService`, `kvClient` and `bridgeServer` modules to the front-end

--- packages/preload/src/index.ts
+++ packages/preload/src/index.ts
@@ -1,5 +1,8 @@
 import { versions } from './versions.js';
 import { ipcRenderer } from 'electron';
+import * as kvStoresService from './kvStoresService.js';
+import * as kvClient from './kvServerClient.js';
+import * as bridgeServer from './server.js';
 
 function send(channel: string, message: string) {
   return ipcRenderer.invoke(channel, message);
@@ -13,4 +16,7 @@ export {
   versions,
   send,
   selectDirectory,
+  kvStoresService,
+  kvClient,
+  bridgeServer,
 };
