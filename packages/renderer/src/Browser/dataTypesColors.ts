78d993922b599d06f7cda741ef0701d8b57781e1
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-08-18 15:23:16 +0300

    Create an object contains custom display colors for different JS data types

---
+++ packages/renderer/src/Browser/dataTypesColors.ts
@@ -0,0 +1,12 @@
+const dataTypesColors: Record<string, string> = {
+    number: "text-[#59a500] dark:text-[#b3ff59]",
+    string: "text-[#c6834e]",
+    boolean: "dark:text-[#6c48b5] text-[#8142ff]",
+    bigint: "dark:text-blue-500 text-blue-800",
+    uint8Array: "text-[#6f4dff] dark:text-[#9981ff]",
+    null: "text-[#006ac7]",
+    undefined: "text-gray-400",
+    regexp: "text-[coral]",
+};
+
+export default dataTypesColors
