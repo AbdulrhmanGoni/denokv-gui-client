d5b9903b0d95ee598ee09030c08c1938f3785b2a
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-08-21 22:57:46 +0300

    Create a header with app's logo and switch theme button
    Create `AppInfo` component which appears as the logo of the
    app and shows app's version and a github repo link button when
    hovering on it.
    
    Then Create `Header` component and put inside it `AppInfo` and
    `SwitchThemeButton` components.

---
+++ packages/renderer/src/lib/components/custom/AppInfo.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import * as Tooltip from "$lib/components/shadcn/tooltip/index.js";
+  import { openExternalLink } from "@app/preload";
+  import Separator from "../shadcn/separator/separator.svelte";
+  import { versions } from "@app/preload";
+  import AppLogo from "./AppLogo.svelte";
+</script>
+
+<Tooltip.Provider delayDuration={0}>
+  <Tooltip.Root>
+    <Tooltip.Trigger>
+      <AppLogo />
+    </Tooltip.Trigger>
+    <Tooltip.Content
+      side="left"
+      align="center"
+      arrowClasses="hidden"
+      class="bg-transparent"
+    >
+      <div class="flex gap-2 items-center h-8">
+        <button
+          class="cursor-pointer"
+          onclick={() => {
+            openExternalLink(versions.appGithubRepo);
+          }}
+        >
+          <img
+            class="size-6 dark:size-0"
+            src="GitHub_Invertocat_Dark.svg"
+            alt="Logo"
+          />
+          <img
+            class="size-0 dark:size-6"
+            src="GitHub_Invertocat_Light.svg"
+            alt="Logo"
+          />
+        </button>
+        <Separator orientation="vertical" />
+        <p class="font-bold text-lg text-foreground">{versions.appVersion}v</p>
+      </div>
+    </Tooltip.Content>
+  </Tooltip.Root>
+</Tooltip.Provider>
---
+++ packages/renderer/src/lib/components/custom/Header.svelte
@@ -0,0 +1,9 @@
+<script>
+  import AppInfo from "./AppInfo.svelte";
+  import SwitchThemeButton from "./SwitchThemeButton.svelte";
+</script>
+
+<div class="my-5 flex justify-between items-center gap-2">
+  <SwitchThemeButton />
+  <AppInfo />
+</div>
