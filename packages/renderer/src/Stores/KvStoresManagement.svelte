bd49bcd0f26ee73f1e082a915c220db7b2bffe67
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-08-21 13:27:59 +0300

    Create "Kv Stores Management" component
    This is the component that holds an manages the rendering of
    `KvStoresGrid`, `AddKvStoreForm` and `EditKvStoreForm`
    components.

---
+++ packages/renderer/src/Stores/KvStoresManagement.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import AddKvStoreForm from "./AddKvStoreForm.svelte";
+  import EditKvStoreForm from "./EditKvStoreForm.svelte";
+  import KvStoresGrid from "./KvStoresGrid.svelte";
+  import { kvStoresState } from "./kvStoresState.svelte";
+</script>
+
+{#if kvStoresState.openAddNewStoreForm}
+  <AddKvStoreForm />
+{:else if kvStoresState.openedStoreToEdit}
+  <EditKvStoreForm kvStore={kvStoresState.openedStoreToEdit} />
+{:else}
+  <KvStoresGrid />
+{/if}
