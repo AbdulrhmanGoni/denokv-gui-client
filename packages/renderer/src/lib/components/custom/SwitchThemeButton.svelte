248a52f49df75aa91a19acd7f59b82523a0a9437
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-08-26 13:42:34 +0300

    Change when each icon appears in `SwitchThemeButton` component

--- packages/renderer/src/lib/components/custom/SwitchThemeButton.svelte
+++ packages/renderer/src/lib/components/custom/SwitchThemeButton.svelte
@@ -12,9 +12,9 @@
   class="p-1.5 size-10"
 >
   <SunIcon
-    class="size-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+    class="size-6 dark:rotate-0 dark:scale-100 transition-all -rotate-90 scale-0"
   />
   <MoonIcon
-    class="absolute size-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+    class="size-6 dark:rotate-90 dark:scale-0 transition-all rotate-0 scale-100 absolute"
   />
 </Button>
