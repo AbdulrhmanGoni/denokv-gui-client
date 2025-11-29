adc78f09623cd6811fbcd5e08b568d57331748c9
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2025-11-29 17:22:41 +0300

    feat: introduce `mac-x64` build support

--- electron-builder.mjs
+++ electron-builder.mjs
@@ -14,6 +14,13 @@ export default /** @type import('electron-builder').Configuration */
       "node_modules/@app/preload/dist/migrations/**",
     ],
     generateUpdatesFilesForAllChannels: true,
+    mac: {
+      target: [
+        { target: "dmg", arch: ["x64", "arm64"] },
+        { target: "zip", arch: ["x64", "arm64"] },
+      ],
+      icon: "buildResources/icon.icns",
+    },
     linux: {
       target: ['deb', 'AppImage'],
       icon: "buildResources",
