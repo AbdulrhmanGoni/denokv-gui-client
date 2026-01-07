e9ffa528a36ec5d434f021d4d12033b2b59b91d0
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2026-01-07 12:36:31 +0300

    chore(fix): quote path to `dbmate` binary to handle potential spaces
    Also use `--no-dump-schema` flag to prevent `dbmate` from trying to dump
    the database schema every time migrations run.

--- packages/preload/src/db/migrate.ts
+++ packages/preload/src/db/migrate.ts
@@ -5,5 +5,5 @@ import path from 'node:path';
 export function migrateDatabaseSchema(dbPath: string) {
     const dbmateBinaryPath = resolveBinary().replace("app.asar", "app.asar.unpacked")
     const migrationsDir = path.join(process.resourcesPath, "app.asar.unpacked/node_modules/@app/preload/dist/migrations")
-    execSync(`${dbmateBinaryPath} -u "sqlite:${dbPath}" -d "${migrationsDir}" migrate`)
+    execSync(`"${dbmateBinaryPath}" -u "sqlite:${dbPath}" -d "${migrationsDir}" --no-dump-schema migrate`)
 }
