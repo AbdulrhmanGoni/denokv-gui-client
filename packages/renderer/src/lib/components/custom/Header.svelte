ad00a5dc7f4ea62be67d9c24a60b9678047a4f4a
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2026-01-11 02:36:47 +0300

    chore: decrease the space between the header what under it

--- packages/renderer/src/lib/components/custom/Header.svelte
+++ packages/renderer/src/lib/components/custom/Header.svelte
@@ -3,7 +3,7 @@
   import AppInfo from "./AppInfo.svelte";
 </script>
 
-<div class="my-5 flex justify-between items-center gap-2">
+<div class="my-2 flex justify-between items-center gap-2">
   <Settings />
   <AppInfo />
 </div>
