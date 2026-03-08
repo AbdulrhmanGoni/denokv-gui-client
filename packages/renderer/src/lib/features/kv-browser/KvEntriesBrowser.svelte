25129c435779dd90e7c95da3da95a6b4316b1a88
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2026-03-08 15:21:44 +0300

    refactor: refactor `onDestroy` call that closes the open Kv Store

--- packages/renderer/src/lib/features/kv-browser/KvEntriesBrowser.svelte
+++ packages/renderer/src/lib/features/kv-browser/KvEntriesBrowser.svelte
@@ -27,12 +27,8 @@
 
   const table = createKvEntriesTable();
 
-  function close() {
-    closeKvStore();
-  }
-
   onDestroy(() => {
-    close();
+    closeKvStore();
     resetEntriesState();
   });
 
