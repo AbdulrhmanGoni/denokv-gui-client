a1c5af2fa6f38a315ccd494c193137223d323391
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2026-05-14 23:58:26 +0300

    chore(landing): upgrade to Astro v6 and update tailwindcss

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -5,10 +5,12 @@ import { readFileSync, writeFileSync } from 'node:fs';
 
 // https://astro.build/config
 export default defineConfig({
+    integrations: [
+        lastModificationDateAutoUpdator()
+    ],
     vite: {
         plugins: [
-            tailwindcss(),
-            lastModificationDateAutoUpdator()
+            tailwindcss()
         ],
     },
     devToolbar: {
@@ -19,21 +21,28 @@ export default defineConfig({
     trailingSlash: `always`
 });
 
+/**
+ * 
+ * @returns {import('astro').AstroIntegration}
+ */
 function lastModificationDateAutoUpdator() {
     return {
         name: "Last Modification Date auto updator",
-        closeBundle: () => {
-            const stimapFilePath = "./dist/sitemap.xml"
-            const xmlFile = readFileSync(stimapFilePath, { encoding: "utf-8" })
-            const currentDate = new Date()
-            writeFileSync(
-                stimapFilePath,
-                xmlFile.replace(
-                    "{{Current Date}}",
-                    `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`
-                ),
-                { encoding: "utf-8" }
-            )
+        hooks: {
+            "astro:build:done": ({ dir }) => {
+                const sitemapFileUrl = new URL("sitemap.xml", dir);
+                const xmlFile = readFileSync(sitemapFileUrl, { encoding: "utf-8" });
+                const currentDate = new Date();
+
+                writeFileSync(
+                    sitemapFileUrl,
+                    xmlFile.replace(
+                        "{{Current Date}}",
+                        `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`
+                    ),
+                    { encoding: "utf-8" }
+                );
+            }
         }
     }
 }
