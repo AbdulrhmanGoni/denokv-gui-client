38a3bdb615a47a3a3443ce42c8833790781c004e
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2026-05-16 01:26:50 +0300

    enhance: improve error handling and feedback clarity of in-app bridge server

--- packages/main/src/modules/bridgeServer.ts
+++ packages/main/src/modules/bridgeServer.ts
@@ -3,7 +3,6 @@ import { AppModule } from "../AppModule.js";
 import { ModuleContext } from "../ModuleContext.js";
 import { type Kv, openKv } from "@deno/kv";
 import { BridgeServerClient, openBridgeServerInNode } from '@app/bridge-server';
-import { type AddressInfo } from "node:net";
 import { randomBytes } from "node:crypto";
 
 class BridgeServerController {
@@ -12,7 +11,7 @@ class BridgeServerController {
     private serverClient: BridgeServerClient | null = null;
     private bridgeServerAuthToken: string | null = null;
 
-    async open(kvStore: KvStore): Promise<void> {
+    async open(kvStore: KvStore): Promise<boolean> {
         await this.close()
 
         if (kvStore.type == "bridge") {
@@ -20,17 +19,24 @@ class BridgeServerController {
             this.serverClient = new BridgeServerClient(kvStore.url, {
                 authToken: this.bridgeServerAuthToken ?? ""
             })
-            return
+            return true
         }
 
         this.bridgeServerAuthToken = randomBytes(30).toString("base64")
         this.kv = await openKv(kvStore.url, { accessToken: kvStore.accessToken })
         this.serverRef = openBridgeServerInNode(this.kv, { port: 0, authToken: this.bridgeServerAuthToken });
 
-        const address = this.serverRef.address() as AddressInfo
+        const address = this.serverRef.address()
+        if (!address) return false
+        if (typeof address === "string") {
+            throw new Error("the in-app bridge server address is not a TCP address, it's either a pipe or a Unix domain socket")
+        }
+
         this.serverClient = new BridgeServerClient(`http://localhost:${address.port}`, {
             authToken: this.bridgeServerAuthToken
         })
+
+        return true
     }
 
     async close(): Promise<void> {
@@ -57,8 +63,7 @@ const bridgeServerController = new BridgeServerController()
 export class BridgeServerModule implements AppModule {
     enable(_context: ModuleContext): void {
         ipcMain.handle("bridgeServer:openServer", async (_, kvStore: KvStore) => {
-            await bridgeServerController.open(kvStore)
-            return true
+            return await bridgeServerController.open(kvStore)
         });
 
         ipcMain.handle("bridgeServer:closeServer", () => bridgeServerController.close());
