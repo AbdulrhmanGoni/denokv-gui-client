02b2b5e843af2fed19452967b193d23e1a2627dc
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2026-06-20 20:13:45 +0300

    chore: upgrade TypeScript from version 5 to 6

--- packages/entry-point.mjs
+++ packages/entry-point.mjs
@@ -15,16 +15,6 @@ if (
   process.on("unhandledRejection", showAndExit);
 }
 
-// noinspection JSIgnoredPromiseFromCall
-/**
- * We resolve '@app/renderer' and '@app/preload'
- * here and not in '@app/main'
- * to observe good practices of modular design.
- * This allows fewer dependencies and better separation of concerns in '@app/main'.
- * Thus,
- * the main module remains simplistic and efficient
- * as it receives initialization instructions rather than direct module imports.
- */
 initApp({
   renderer:
     process.env.MODE === "development" && !!process.env.VITE_DEV_SERVER_URL
