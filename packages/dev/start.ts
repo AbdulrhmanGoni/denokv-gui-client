d522a65d4cad2e195ac93f5e99ec4741af5d0a33
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2026-09-15 17:49:13 +0300

    refactor: create a dedicated package for the development mode

--- packages/dev/start.ts
+++ packages/dev/start.ts
@@ -1,4 +1,6 @@
-import { build, createServer } from "vite";
+#!/usr/bin/env node
+
+import { build, createServer, type Plugin } from "vite";
 import path from "path";
 
 const mode = "development";
@@ -9,7 +11,6 @@ process.env.MODE = mode;
  * A development server for the renderer. This server should be started first because
  * other packages depend on its settings.
  */
-/** @type {import("vite").ViteDevServer} */
 const rendererWatchServer = await createServer({
   mode,
   root: path.resolve("packages/renderer"),
@@ -21,13 +22,10 @@ await rendererWatchServer.listen();
  * A simple provider plugin to provide access to the renderer dev-server to all other
  * build processes.
  */
-/** @type {import("vite").Plugin} */
-const rendererWatchServerProvider = {
+const rendererWatchServerProvider: Plugin = {
   name: "@app/renderer-watch-server-provider",
   api: {
-    provideRendererWatchServer() {
-      return rendererWatchServer;
-    },
+    provideRendererWatchServer: () => rendererWatchServer,
   },
 };
 
@@ -35,8 +33,6 @@ const rendererWatchServerProvider = {
  * Building all other packages. For each of them, we add a plugin provider so that each
  * package can implement its own hot update mechanism.
  */
-
-/** @type {string[]} */
 const packagesToStart = ["packages/bridge-server", "packages/preload", "packages/main"];
 
 for (const pkg of packagesToStart) {
