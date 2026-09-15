cd65f5eee880b3f40c5c850f56bc7fd58201edaa
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2026-09-15 14:36:54 +0300

    refactor: move the declaration of `APP_VERSION` and `APP_GITHUB_REPO` to the main package

---
+++ packages/main/global.d.ts
@@ -0,0 +1,5 @@
+/** A global variable that holds the current application version */
+declare const APP_VERSION: string;
+
+/** A global variable that holds the github repository url of the application */
+declare const APP_GITHUB_REPO: string;
